e220dafe09878d58d170009dc6dcdc4b6e8701cb
Boris Krasko <59699177+boriskrasko@users.noreply.github.com>
2020-10-03 19:53:51 +0000

    feat: add 8-kyu_palindrome-strings.js

--- 8-kyu_palindrome-strings.js
+++ 8-kyu_palindrome-strings.js
@@ -1,4 +1,5 @@
-// A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. 
+// A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+
 // This includes capital letters, punctuation, and word dividers.
 
 // Implement a function that checks if something is a palindrome.
