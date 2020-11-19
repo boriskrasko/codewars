4a89da3922a7c637dcbcbf661df9af0fa9ed6b5e
Boris Krasko <boris.krasko@icloud.com>
2020-11-19 21:09:27 +0100

    feat: del old solutions

--- 8-kyu_even-or-odd.js
+++
@@ -1,8 +0,0 @@
-//Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-
-function even_or_odd(number) {
-  if (number % 2) {
-    return "Odd";
-  }
-  return "Even";
-}
